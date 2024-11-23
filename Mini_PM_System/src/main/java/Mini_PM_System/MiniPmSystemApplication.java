53b6993721a6275a18431af8fcbae69a8f72d801
JinA <JinA@DESKTOP-IK7R8TA>
2024-11-23 16:48:17 +0900

    시작

--- Mini_PM_System/src/main/java/Mini_PM_System/MiniPmSystemApplication.java
+++ Mini_PM_System/src/main/java/Mini_PM_System/MiniPmSystemApplication.java
@@ -2,12 +2,20 @@ package Mini_PM_System;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootApplication
+@Controller
 public class MiniPmSystemApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MiniPmSystemApplication.class, args);
+	}
+	
+	@RequestMapping("/")
+	public String index() {
+		return "thymeleaf/index";
 	}
 
 }
