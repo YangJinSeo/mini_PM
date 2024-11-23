0b140c1274fe628419fd27934ef448b5e7e5f7b0
YangJinSeo <wls00tj0827@gmail.com>
2024-11-23 15:42:09 +0900

    프로젝트 시작

---
+++ Mini_PM_System/src/main/java/Mini_PM_System/MiniPmSystemApplication.java
@@ -0,0 +1,13 @@
+package Mini_PM_System;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniPmSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniPmSystemApplication.class, args);
+	}
+
+}
---
+++ Mini_PM_System/src/test/java/Mini_PM_System/MiniPmSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package Mini_PM_System;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniPmSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
