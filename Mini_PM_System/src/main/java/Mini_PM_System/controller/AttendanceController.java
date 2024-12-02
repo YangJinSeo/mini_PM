2e10d4179c2bdb13632b02ae6b8ed499c71fa9d0
YangJinSeo <wls00tj0827@gmail.com>
2024-12-02 15:09:12 +0900

    근태 관리

--- Mini_PM_System/src/main/java/Mini_PM_System/controller/AttendanceController.java
+++ Mini_PM_System/src/main/java/Mini_PM_System/controller/AttendanceController.java
@@ -1,14 +1,26 @@
 package Mini_PM_System.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import Mini_PM_System.service.attendence.AttListService;
 
 @Controller
 @RequestMapping("attendance")
 public class AttendanceController {
+	@Autowired
+	AttListService attListService;
+	
 	
 	@RequestMapping("attList")
-	public String attList() {
+	public String attList(@RequestParam(value = "page", required = true, defaultValue = "1") int page
+			,@RequestParam(value = "searchWord", required = false) String searchWord
+			,Model model) {
+		attListService.execute(page, searchWord, model);
 		return "thymeleaf/attendance/attList";
 	}
+	
 }
